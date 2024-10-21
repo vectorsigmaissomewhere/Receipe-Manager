07bf7f6da6bb8205aa1b3f9b55ed906b52506bbd
whereyoucanfindme <anishchy501625@gmail.com>
2024-10-21 08:00:00 +0545

    first commit

--- core/home/serializers.py
+++ core/home/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Receipe, Ingredients
+from django.utils.text import slugify
+import uuid
 
 class IngredientsSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,18 +10,38 @@ class IngredientsSerializer(serializers.ModelSerializer):
 
 class ReceipeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Receipe 
-        # fields = '__all__' # get all the field 
-        fields =["receipe_name", "receipe_description"]
+        model = Receipe
+        fields = ["receipe_name", "receipe_description"]
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
         data['ingredients'] = IngredientsSerializer(
             instance.receipe_ingredients.all(), many=True).data
-        return data 
+        return data
 
 class CreateReceipeSerializer(serializers.ModelSerializer):
     receipe_slug = serializers.CharField(allow_null=True, required=False)
+    receipe_ingredients = serializers.ListField(
+        child=serializers.CharField()
+    )
+
     class Meta:
         model = Receipe
-        fields = '__all__'
+        exclude = ['receipe_image']
+
+    def create(self, validated_data):
+        receipe_slug = slugify(validated_data['receipe_name'])
+        if Receipe.objects.filter(receipe_slug=receipe_slug).exists():
+            receipe_slug = f"{receipe_slug}_{str(uuid.uuid4()).split('-')[0]}"
+        
+        receipe = Receipe.objects.create(
+            receipe_name=validated_data['receipe_name'],
+            receipe_description=validated_data['receipe_description'],
+            receipe_slug=receipe_slug,
+            receipe_type=validated_data['receipe_type'],
+        )
+        
+        for ri in validated_data.get('receipe_ingredients'):
+            Ingredients.objects.create(receipe=receipe, ingredient_name=ri)
+
+        return receipe
--- core/home/views.py
+++ core/home/views.py
@@ -1,32 +1,63 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import ReceipeSerializer, CreateReceipeSerializer
 from .models import Receipe, Ingredients
+import logging
+
+logger = logging.getLogger(__name__)
 
 class ReceipeAPI(APIView):
     def get(self, request):
         queryset = Receipe.objects.all()
-        print(queryset)
+        logger.debug(queryset)
         serializer = ReceipeSerializer(queryset, many=True)
-        print(serializer.data)
+        logger.debug(serializer.data)
         return Response({
             "status": True,
-            "message": "data fetched",
+            "message": "Data fetched",
             "data": serializer.data
-        })
-    
+        }, status=status.HTTP_200_OK)
+
     def post(self, request):
-        data = request.data 
+        data = request.data
         serializer = CreateReceipeSerializer(data=data)
         if not serializer.is_valid():
             return Response({
                 "status": False,
-                "message": "data not created",
+                "message": "Data not created",
                 "data": serializer.errors
-            })
-        
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        receipe = serializer.save()
         return Response({
-            "status": True, 
-            "message": "data created",
+            "status": True,
+            "message": "Recipe created",
+            "data": ReceipeSerializer(receipe).data  # Return serialized data
+        }, status=status.HTTP_201_CREATED)
+
+    def delete(self, request):
+        data = request.data
+        recipe_id = data.get('id')
+
+        if not recipe_id:
+            return Response({
+                "status": False,
+                "message": "ID not provided",
+                "data": {}
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        receipe = Receipe.objects.filter(id=recipe_id)
+        if receipe.exists():
+            receipe.delete()
+            return Response({
+                "status": True,
+                "message": "Recipe deleted",
+                "data": {}
+            }, status=status.HTTP_200_OK)
+
+        return Response({
+            "status": False,
+            "message": "Recipe not found",
             "data": {}
-        })
+        }, status=status.HTTP_404_NOT_FOUND)
